32db12e759f9613b9332f60196fb2fa4188c3570
Michael Jurkoic <mjjurkoic@gmail.com>
2019-10-05 20:14:04 -0500

    Added JSON parsing

--- MercuryBrowser/ViewController.swift
+++ MercuryBrowser/ViewController.swift
@@ -12,18 +12,46 @@ class ViewController: UIViewController {
 
     let projectURLString = "https://raw.githubusercontent.com/rmirabelli/mercuryserver/master/mercury.json"
     
-    struct ProjectData: Codable {
-        let projectDataList: [[String]]
+    struct MercuryData: Codable {
+        let mercury: [Dictionary<String, String>]
     }
+    
+    struct MercuryType {
+        let name: String
+        let type: String
+        let url: String
+        
+        init(inputDict: Dictionary<String, String>) {
+            name = inputDict["name"]!
+            type = inputDict["type"]!
+            url = inputDict["url"]!
+        }
+    }
+    
+    var mercuryArray: [MercuryType] = []
+    
+    @IBOutlet weak var mercuryTable: UITableView!
+    @IBOutlet weak var mercuryCell: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let session = URLSession(configuration: .ephemeral)
         let task = session.dataTask(with: URL(string: projectURLString)!) { (data, response, error) in
-            let projectData = try! JSONDecoder().decode(ProjectData.self, from: data!)
+            if let data = data {
+                let projectData = try! JSONDecoder().decode(MercuryData.self, from: data)
+                for item in projectData.mercury {
+                    self.mercuryArray.append(MercuryType(inputDict: item))
+                }
+                // Helper function defined elsewhere in ViewController
+                self.fillTableView(dataArray: self.mercuryArray)
+            }
         }
+        task.resume()
     }
 
+    func fillTableView(dataArray: [MercuryType]) {
+        
+    }
 
 }
